ff0239e09dfe4d16f1ef53305f3a54e8a9dca983
Erik Zigo <info@spoluprace.net>
2017-10-18 11:45:31 +0200

    Update main.php

--- main.php
+++ main.php
@@ -4,14 +4,3 @@ require "vendor/autoload.php";
 
 echo "ahoj";
 exit(0);
-
-
-try {
-
-} catch (InvalidArgumentException $e) {
-    echo $e->getMessage();
-    exit(1);
-} catch (\Throwable) {
-    echo $e->getMessage();
-    exit(2);
-}
